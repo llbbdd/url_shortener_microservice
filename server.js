3a4cc5398726465e09242d7840ed6b2f42e0ebd3
generalwellbeing <leighbrodie@gmail.com>
2017-04-01 07:56:11 +0000

    URL validation added using valid-url package

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var mongo = require('mongodb');
+var validUrl = require('valid-url');
 
 var port = 8080;
 var mongoDatabase = "mongodb://localhost:27017/shorturl";
@@ -10,14 +11,22 @@ mongo.MongoClient;
 
 app.get('/new/:url', function(req, res) {
     var originalUrl = req.params.url;
-    var shortUrl;
-    console.log(originalUrl);
-    addSite("www.bbc.co.uk", "shorturl");
     
     res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({ "original_url": originalUrl, "short_url": shortUrl}));
     
-    showDB();
+    if (typeof validUrl.isUri(originalUrl) === 'undefined'){
+        console.log("not adding");
+        res.send(JSON.stringify({ "original_url": "error", "short_url": "error"}));
+    }
+    else{
+        var shortUrl;
+    
+        addSite(originalUrl, "shorturl");
+        
+        res.send(JSON.stringify({ "original_url": originalUrl, "short_url": shortUrl}));
+        
+        showDB();
+    }
 });
 
 app.listen(port, function () {
